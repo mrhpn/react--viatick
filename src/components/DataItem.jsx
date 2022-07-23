038a8432613c6e8760ef7bace975e1d225702e30
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-07-23 21:34:25 +0630

    finished camera icons colored

--- src/components/DataItem.jsx
+++ src/components/DataItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function DataItem({ Icon, name, count, color = 'black' }) {
   return (
-    <div className="inline-flex items-center p-2 px-3 m-2 border shadow-md rounded-lg">
+    <div className="inline-flex items-center p-2 px-3 m-2 border shadow-md rounded-lg bg-white">
       <Icon size={28} style={{ color }} />
       <div className="flex-col pl-3">
         <div className="font-bold" style={{ color }}>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { Icon, DivIcon } from 'leaflet';
+
+const customIcon = new Icon({
+  iconUrl: '/camera.svg',
+  iconSize: [23, 23],
+});
+
+const iconRed = new DivIcon({
+  className: 'red-marker',
+  iconSize: [23, 23],
+});
+
+const iconBlack = new DivIcon({
+  className: 'black-marker',
+  iconSize: [23, 23],
+});
+
+const iconPurple = new DivIcon({
+  className: 'purple-marker',
+  iconSize: [23, 23],
+});
 
 function Map({ pins }) {
+  //let long, lang;
+
   return (
     <MapContainer
       center={[1.3644, 103.75259]}
@@ -13,8 +36,27 @@ function Map({ pins }) {
       />
       {pins.length &&
         pins.map((pin) => (
-          <Marker key={pin.pinId} position={[pin.long, pin.lang]}>
-            <Popup>{pin.name}</Popup>
+          <Marker
+            key={pin.pinId}
+            position={[pin.long, pin.lang]}
+            icon={pin.isAlert ? iconRed : iconBlack}>
+            <Popup>
+              <div>
+                <h2 className="font-bold h1">{pin.name}</h2>
+                <span className="block italic text-xs text-gray-500">
+                  Department: {pin.department.name}
+                </span>
+                <span className="block italic text-xs text-gray-500">
+                  Group: {pin.group.name}
+                </span>
+                <span className="text-gray-700 mt-3 block">{pin.remarks}</span>
+                <img
+                  className="block mt-3 rounded"
+                  src={pin.image}
+                  alt={pin.name}
+                />
+              </div>
+            </Popup>
           </Marker>
         ))}
     </MapContainer>
