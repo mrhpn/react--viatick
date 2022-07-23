d548be1b89c6b482b62480f767202cf19688fef1
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-07-23 16:44:53 +0630

    configured basic map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import { HiCamera, HiHand } from 'react-icons/hi';
 import DataItem from './components/DataItem';
+import Map from './components/Map';
 
 function App() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto mt-10">
       <div className="row-auto">
         <DataItem Icon={HiCamera} name="Camera" count={11} />
         <DataItem Icon={HiHand} name="Active Alert" count={2} color="red" />
+      </div>
+      <div
+        id="map"
+        className="row-auto m-2 border shadow-md rounded-lg leaflet-container">
+        <Map />
       </div>
     </div>
   );
